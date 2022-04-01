d7ceb6432ebf3b2fb6ab436964d4279c8251ba89
Chaussure <ocelot3mwa@hotmail.fr>
2022-04-01 21:09:49 +0200

    Load_interface_2.2

---
+++ jeu.c
@@ -0,0 +1,11 @@
+#ifdef __APPLE_CC__
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <stdlib.h>
+#include <stdio.h>
+#include "jeu.h"
+//#include "LoadMaps.h"
+
