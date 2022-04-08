54966aff96ba387a7d95128eab1e031746d03ec2
FrancoisCransveld <93730368+FrancoisCransveld@users.noreply.github.com>
2022-04-08 03:51:11 +0200

    Update ennemi.h

--- ennemi.h
+++ ennemi.h
@@ -29,4 +29,10 @@ struct elementEnnemi{
   ElementEnnemi* dernierListe;
 };
 
+ListeEnnemi* creer_liste();
+void nouvel_ennemi(ListeEnnemi* liste, char nom[], int vie, Coordonnee pos, TypeEnnemi t);
+void modifier_ennemi(ListeEnnemi* liste, int numero, char nom[], int vie, Coordonnee pos, TypeEnnemi t);
+void supprimer_ennemi_numero(ListeEnnemi* liste, int numero);
+void retirer_vie_numero(ListeEnnemi* liste, int numero, int degat);
+void modifier_pos_ennemi(ListeEnnemi* liste, int numero, Coordonnee pos);
 #endif
